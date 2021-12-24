2705626cbf48c6d5b6e054e3ef526b817f21675a
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-24 15:26:39 +0100

    adding new button, fix positon and flexbox

--- carousel/app.js
+++ carousel/app.js
@@ -1,3 +1,13 @@
-const slides = document.getElementsByTagName('img');
+const slides = document.querySelector(".slides");
 
+function changeSlide () {
+    slides.style.left = '-110vw';
+}
+
+document.querySelector(".next-button").addEventListener("click", function() {
+    changeSlide();
+});
+
+
+//tant que compteurImage 
 
