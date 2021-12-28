c46ceb746870cfbccee89a22aa950a147db7d4aa
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-28 15:01:31 +0100

    adding variable in js

---
+++ scrore-keeper/app.js
@@ -0,0 +1,8 @@
+let numPlayer1 = document.querySelector("#num-player1");
+let numPlayer2 = document.querySelector("#num-player2");
+
+let buttonPlayer1 = document.querySelector("#button-player1");
+let buttonPlayer2 = document.querySelector("#button-player2");
+let buttonReset = document.querySelector("#button-reset");
+
+
