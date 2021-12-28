3e4c46dcf80da9064f9c97a62aaef7e15e64301f
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-28 13:46:33 +0100

    adding start test

--- shopping-list/app.js
+++ shopping-list/app.js
@@ -1,13 +1,10 @@
 let list = document.getElementById('shopping-list');
 
-function addingText() {
+/*export*/ function addingText() {
     let addText = document.getElementById('input-add-text').value;
     let entry = document.createElement('li');
     if (addText.replace(/\s+/, '').length) {
         entry.appendChild(document.createTextNode(addText));
         list.appendChild(entry);
     } 
-   }
-    
-
-
+}
