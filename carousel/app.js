9f49281409b9509918128093c63e2d5bd371e323
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-28 10:32:32 +0100

    changing js, move slides is ok

--- carousel/app.js
+++ carousel/app.js
@@ -1,12 +1,11 @@
 const slides = document.querySelector(".slides");
 const slidesCount = slides.childElementCount;
-let sizeMove = -80;
+const sizeMove = -80;
 let move = 0;
 
 function changeSlide () {
     move = move + sizeMove
-    console.log(move);
-    slides.style.left = sizeMove + "em";
+    slides.style.left = move + "em";
 }
 
 document.querySelector(".next-button").addEventListener("click", function() {
