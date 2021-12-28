f07591cd0bb9e49b316d3f8cf218ba596bd80142
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-28 11:15:42 +0100

    adding if else for slides move

--- carousel/app.js
+++ carousel/app.js
@@ -1,13 +1,22 @@
 const slides = document.querySelector(".slides");
 const slidesCount = slides.childElementCount;
+const maxLeft = (slidesCount - 1) * 80 * -1;
 const sizeMove = -80;
 let move = 0;
 
-function changeSlide () {
-    move = move + sizeMove
-    slides.style.left = move + "em";
+function changeSlide (next = true) {
+    if (next) {
+        move += move > maxLeft ? -80 : move * -1;
+      } else {
+        move = move < 0 ? move + 80 : maxLeft;
+      }
+      slides.style.left = move + "em";
 }
 
 document.querySelector(".next-button").addEventListener("click", function() {
     changeSlide();
+});
+
+document.querySelector(".previous-button").addEventListener("click", function() {
+    changeSlide(false);
 });
