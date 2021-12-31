44bb62ab1e6bc20a4d09de7ce14aac925b0265ff
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-31 14:07:15 +0100

    adding function change color

--- scrore-keeper/app.js
+++ scrore-keeper/app.js
@@ -1,21 +1,26 @@
-let numPlayer1 = document.querySelector("#num-player1");
-let numPlayer2 = document.querySelector("#num-player2");
-let buttonPlayer1 = document.querySelector("#button-player1");
-let buttonPlayer2 = document.querySelector("#button-player2");
-let buttonReset = document.querySelector("#button-reset");
+let player1Button = document.querySelector(".button-player1");
+let player2Button = document.querySelector(".button-player2");
+let resetButton = document.querySelector(".button-reset");
+
+let player1Score = document.querySelector(".num-player1");
+let player2Score = document.querySelector(".num-player2");
+
+let numInput = document.querySelector(".input-num");
+
+let winnerNum = document.querySelector(".num-winner");
 
 let score1 = 0;
-let score2 = 0;
 
-console.log(numPlayer1);
+// contenu input afficher dans la span 'num-winner'
+numInput.addEventListener("change", function(){
+    winnerNum.textContent=this.value;        
+    }
+);
 
-score1 =+ 1;
-console.log(score1);
-
-
-let replacedNode = numPlayer1.replaceChild(score1, numPlayer1);
-console.log(replacedNode);
-
-// document.querySelector("#button-player1").addEventListener("click", function() {
-//     ();
-// });
+function change () {
+    winnerNum.style.color = "green";
+}
+    
+document.querySelector(".button-player1").addEventListener("click", function() {
+    change();
+});
