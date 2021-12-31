28d137590d34634224c4ce6f4777d1317c94cfb7
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-31 10:16:23 +0100

    delete test

--- shopping-list/test/app.test.js
+++
@@ -1,9 +0,0 @@
-import chai from 'chai';
-const assert = chai.assert;
-import {addingText} from '../app.js';
-
-describe('Slide mouvement', () => {
-    it('should return ...', () => {
-        assert.isOk(addingText(), 'azerty');
-    })
-}) 
