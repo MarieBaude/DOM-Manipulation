fa2e0f0bf0d267f3acb1c2459fbd70dd92d7017e
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-31 17:19:29 +0100

    try to compared values

--- scrore-keeper/app.js
+++ scrore-keeper/app.js
@@ -1,3 +1,6 @@
+/* VARIABLES 
+-----------------------------
+*/
 let player1Score = document.querySelector(".num-player1");
 let player2Score = document.querySelector(".num-player2");
 
@@ -11,26 +14,41 @@ let resetButton = document.querySelector(".button-reset");
 let clicksPlayer1 = 0;
 let clicksPlayer2 = 0;
 
+/* 
+-----------------------------
+*/
+
+numInput.addEventListener("change", function() {
+    winnerNum.textContent=this.value;
+    console.log(winnerNum);        
+});
+
+player1Button.addEventListener("click", function() {
+    addClickPlayer1();
+    comparedValues();
+});
+
+player2Button.addEventListener("click", function() {
+    addClickPlayer2();
+});
+
+/* FUNCTIONS 
+-----------------------------
+*/
 function addClickPlayer1 () {
     clicksPlayer1++;
-    document.querySelector(".num-player1").innerHTML = clicksPlayer1;
+    player1Score.innerHTML = clicksPlayer1;
 }
 
 function addClickPlayer2 () {
     clicksPlayer2++;
-    document.querySelector(".num-player2").innerHTML = clicksPlayer2;
+    player2Score.innerHTML = clicksPlayer2;
 }
 
-// contenu input afficher dans la span 'num-winner'
-numInput.addEventListener("change", function(){
-    winnerNum.textContent=this.value;        
+function comparedValues () {
+    if (clicksPlayer1 === winnerNum) {
+        console.log('yes');
+    } else {
+        console.log('no');
     }
-);
-
-document.querySelector(".button-player1").addEventListener("click", function() {
-    addClickPlayer1();
-});
-
-document.querySelector(".button-player2").addEventListener("click", function() {
-    addClickPlayer2();
-});
+}
