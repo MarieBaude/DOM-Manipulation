b32dae65b0d4949729de71369c41eff175f67247
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-31 14:55:47 +0100

    adding note algo

--- scrore-keeper/app.js
+++ scrore-keeper/app.js
@@ -1,15 +1,12 @@
-let player1Button = document.querySelector(".button-player1");
-let player2Button = document.querySelector(".button-player2");
-let resetButton = document.querySelector(".button-reset");
-
 let player1Score = document.querySelector(".num-player1");
 let player2Score = document.querySelector(".num-player2");
 
+let winnerNum = document.querySelector(".num-winner");
 let numInput = document.querySelector(".input-num");
 
-let winnerNum = document.querySelector(".num-winner");
-
-let score1 = 0;
+let player1Button = document.querySelector(".button-player1");
+let player2Button = document.querySelector(".button-player2");
+let resetButton = document.querySelector(".button-reset");
 
 // contenu input afficher dans la span 'num-winner'
 numInput.addEventListener("change", function(){
