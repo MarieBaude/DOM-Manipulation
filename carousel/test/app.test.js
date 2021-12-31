ff2c50747fdc58418ae0e37b906069ffac97fd14
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-31 10:15:12 +0100

    delete test

--- carousel/test/app.test.js
+++
@@ -1,9 +0,0 @@
-import chai from 'chai';
-const assert = chai.assert;
-import {changeSlide} from '../app.js';
-
-describe('Slide mouvement', () => {
-    it('should return ...', () => {
-        assert.isOk(changeSlide(), 'azerty');
-    })
-}) 
--- scrore-keeper/app.js
+++ scrore-keeper/app.js
@@ -1,8 +1,21 @@
 let numPlayer1 = document.querySelector("#num-player1");
 let numPlayer2 = document.querySelector("#num-player2");
-
 let buttonPlayer1 = document.querySelector("#button-player1");
 let buttonPlayer2 = document.querySelector("#button-player2");
 let buttonReset = document.querySelector("#button-reset");
 
+let score1 = 0;
+let score2 = 0;
 
+console.log(numPlayer1);
+
+score1 =+ 1;
+console.log(score1);
+
+
+let replacedNode = numPlayer1.replaceChild(score1, numPlayer1);
+console.log(replacedNode);
+
+// document.querySelector("#button-player1").addEventListener("click", function() {
+//     ();
+// });
