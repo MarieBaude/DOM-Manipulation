666c31209c89f72282bcdc769c4352638bc8b145
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-31 15:33:38 +0100

    adding function display number of click

--- scrore-keeper/app.js
+++ scrore-keeper/app.js
@@ -8,16 +8,29 @@ let player1Button = document.querySelector(".button-player1");
 let player2Button = document.querySelector(".button-player2");
 let resetButton = document.querySelector(".button-reset");
 
+let clicksPlayer1 = 0;
+let clicksPlayer2 = 0;
+
+function addClickPlayer1 () {
+    clicksPlayer1++;
+    document.querySelector(".num-player1").innerHTML = clicksPlayer1;
+}
+
+function addClickPlayer2 () {
+    clicksPlayer2++;
+    document.querySelector(".num-player2").innerHTML = clicksPlayer2;
+}
+
 // contenu input afficher dans la span 'num-winner'
 numInput.addEventListener("change", function(){
     winnerNum.textContent=this.value;        
     }
 );
 
-function change () {
-    winnerNum.style.color = "green";
-}
-    
 document.querySelector(".button-player1").addEventListener("click", function() {
-    change();
+    addClickPlayer1();
+});
+
+document.querySelector(".button-player2").addEventListener("click", function() {
+    addClickPlayer2();
 });
