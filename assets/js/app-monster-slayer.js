b845a25b2c95cec7c53784909c0e9bb208e4a697
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-07 12:13:35 +0100

    change name base file in commun, adding color button monster slayer

--- assets/js/app-monster-slayer.js
+++ assets/js/app-monster-slayer.js
@@ -1,15 +1,29 @@
 /* VARIABLES 
 -----------------------------
 */
+const buttonStart = document.querySelector('.new-start-monster');
+const buttonAttack = document.querySelector('.attack');
+const buttonSpecialAttack = document.querySelector('.special-attack');
+const buttonHeal = document.querySelector('.heal');
+const buttonGiveUP = document.querySelector('.give-up');
 
 
-/* 
+/* EVENTS
 -----------------------------
 */
-
+buttonStart.addEventListener('click', function() {
+    appearButton();
+});
 
 
 /* FUNCTIONS 
 -----------------------------
 */
 
+function appearButton () {
+    buttonStart.style.display = 'none';
+    buttonAttack.style.display = 'inher';
+    buttonSpecialAttack.style.display = 'inher';
+    buttonHeal.style.display = 'inher';
+    buttonGiveUP.style.display = 'inher';
+}
